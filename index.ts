8d9269ae7837eeec36b6fded21ccd6418adc6771
Gazi-Md-Salauddin <mdsalauddin329132@gmail.com>
2026-07-15 20:15:15 +0600

    fix api issue

--- index.ts
+++ index.ts
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 // type fix
-const port = Number(process.env.NEXT_PUBLIC_BASE_URL || 5000);
+const port = Number(process.env.PORT || 5000);
 
 const uri = process.env.MONGODB_URI;
 if (!uri) {
